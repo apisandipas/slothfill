ea11d57760832bf90fcc66e633e6919dec472be8
Bryan Paronto <bparonto@gmail.com>
2013-06-19 19:53:29 -0500

    adde img magick

--- Gemfile
+++ Gemfile
@@ -1,7 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'thin',                 '~> 1.5.1'
-gem 'sinatra',              '~> 1.4.2', :require => 'sinatra/base'
+gem 'thin',     '~> 1.5.1'
+gem 'sinatra',  '~> 1.4.2',  :require => 'sinatra/base'
+gem 'rmagick',  '~> 2.13.1', :require => 'RMagick'
 
 group :development do
   gem 'shotgun',            '~> 0.9'
--- app.rb
+++ app.rb
@@ -1,13 +1,45 @@
-require "bundler/setup"
-require "sinatra"
+class App < Sinatra::Base
+  include Magick
 
-get '/' do
-  erb :index
-end
+  get '/' do
+    erb :index
+  end
 
-get '/:width/:height' do
-    width = params[:width]
-    height = params[:height]
+  get '/:width/:height' do
+      # grab params
+      width = params[:width]
+      height = params[:height]
+
+      raise Sinatra::NotFound unless width.match(/\A[0-9]+\Z/) and height.match(/\A[0-9]+\Z/)
+
+      # Cast as Integers
+      width = Integer(width)
+      height = Integer(height)
+
+
+      sloths = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six'
+      ] 
+
+      sloth = sloths.sample
+
+      content_type 'image/jpg'
+
+      image = Magick::Image.read("./sloths/#{sloth}.jpg").first
+      image = image.resize_to_fill(width, height)
+      image.format = 'jpg'
+      image.to_blob
+
+  end
+
+  not_found do
+    erb :not_found
+  end
 
 
 end
