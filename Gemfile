91ef979fa6f02378432167b377b591c620069542
Bryan Paronto <bparonto@gmail.com>
2013-06-24 23:01:34 -0500

    added cacheing for image sizes

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
 source 'https://rubygems.org'
 
-gem 'thin',     '~> 1.5.1'
-gem 'sinatra',  '~> 1.4.2',  :require => 'sinatra/base'
-gem 'rmagick',  '~> 2.13.1', :require => 'RMagick'
+gem 'thin',        '~> 1.5.1'
+gem 'sinatra',     '~> 1.4.2',  :require => 'sinatra/base'
+gem 'mini_magick', '~> 3.6.0'
 
 group :development do
   gem 'shotgun',            '~> 0.9'
--- app.rb
+++ app.rb
@@ -1,44 +1,55 @@
 class App < Sinatra::Base
-  include Magick
+  include MiniMagick
 
   get '/' do
     erb :index
   end
 
   get '/:width/:height' do
-      # grab params
-      width = params[:width]
-      height = params[:height]
+    
+    # grab params
+    width = params[:width]
+    height = params[:height]
 
-      raise Sinatra::NotFound unless width.match(/\A[0-9]+\Z/) and height.match(/\A[0-9]+\Z/)
+    raise Sinatra::NotFound unless width.match(/\A[0-9]+\Z/) and height.match(/\A[0-9]+\Z/)
 
-      # Cast as Integers
-      width = Integer(width)
-      height = Integer(height)
+    # Cast as Integers
+    width = Integer(width)
+    height = Integer(height)
 
-
-      sloths = [
-        'one',
-        'two',
-        'three',
-        'four',
-        'five',
-        'six'
-      ] 
-
-      sloth = sloths.sample
-
-      content_type 'image/jpg'
-
-      image = Magick::Image.read("./sloths/#{sloth}.jpg").first
-      image = image.resize_to_fill(width, height)
-      image.format = 'jpg'
-      image.to_blob
+    if File.exists? "./sloths/cache/#{width}_#{height}.jpg"
+      grab_from_cache(width, height)
+    else 
+      create_new_image(width, height)
+    end
 
   end
 
   not_found do
     erb :not_found
+  end
+
+  def grab_from_cache(width, height)
+    puts "Cached File available"
+    sloth = File.open "./sloths/cache/#{width}_#{height}.jpg"
+    puts "Here be the sloth: #{sloth.to_s}" 
+    send_file(sloth)
+  end
+
+  def create_new_image(width, height)
+    puts "Creating new image..."
+
+    sloths = %w[one two three four five six] 
+    sloth = sloths.sample 
+
+    content_type 'image/jpg'
+    image = MiniMagick::Image.open("./sloths/#{sloth}.jpg")
+    image.resize("#{width}x#{height}")
+
+    puts 'Cacheing new image for future use..'
+    image.write("./sloths/cache/#{width}_#{height}.jpg")
+
+    image.to_blob
   end
 
 
